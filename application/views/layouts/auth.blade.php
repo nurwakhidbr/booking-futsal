2fdb269e20657da784824723d7efa424cb06f0cc
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2020-11-13 14:32:13 +0700

    fix fontawesome loaded

--- application/views/layouts/auth.blade.php
+++ application/views/layouts/auth.blade.php
@@ -5,11 +5,11 @@
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                     <div class="login-brand">
-                        <img src="assets/img/logo.png" alt="logo" width="100" class="shadow-light rounded-circle">
+                        <img src="assets/img/logo.png" alt="logo" width="100" class="rounded-circle">
                     </div>
                     @yield('content')
                     <div class="mt-5 text-muted text-center">
-                    Don't have an account? <a href="auth-register.html">Create One</a>
+                    Don't have an account? <a href="#">Create One</a>
                     </div>
                     <div class="simple-footer">
                     Copyright &copy; AM4K {{ date('Y') }}
--- application/views/layouts/skeleton.blade.php
+++ application/views/layouts/skeleton.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Home' }} | SIFutsal</title>
+    <link rel="shortcut icon" href="{{ base_url('assets/img/favicon.ico') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ base_url('assets/img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ base_url('assets/img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ base_url('assets/img/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ base_url('assets/site.webmanifest') }}">
     <link rel="stylesheet" href="{{ base_url('assets/css/app.css') }}">
 </head>
 <body>
