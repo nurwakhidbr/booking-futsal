6be267559d47f85165919e4207af3ad81af4ba28
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2020-11-15 13:05:19 +0700

    redesign folder structur for admin & operator

---
+++ application/views/_admin/booking/data.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Booking</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item">Master Data</div>
+                <div class="breadcrumb-item active"><a href="">Booking</a></div>
+            </div>
+        </div>
+        <div class="section-body">
+        </div>
+    </section>
+@endsection
---
+++ application/views/_partials/sidebar.blade.php
@@ -0,0 +1,53 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="{{ base_url('dashboard') }}">SIFUT</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a href="">SIF</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">dashboard</li>
+        <li class="active">
+            <a class="nav-link" href="{{ base_url('dashboard') }}">
+                <i class="fas fa-home"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="menu-header">Master Data</li>
+        <li class="dropdown">
+            <a class="nav-link has-dropdown" href="">
+                <i class="fas fa-database"></i>
+                <span>Master Data</span>
+            </a>
+            <ul class="dropdown-menu">
+                {{-- jika login role nya operator & admin --}}
+                <li><a href="#" class="nav-link">Data Booking</a></li>
+                <li><a href="#" class="nav-link">Data Member</a></li>
+                <li><a href="#" class="nav-link">Data User</a></li>
+                <li><a href="#" class="nav-link">Data Lapangan</a></li>
+                <li><a href="#" class="nav-link">Data Harga</a></li>
+                {{-- jika login role nya administrator saja --}}
+                <li><a href="#" class="nav-link">Data Operator</a></li>
+            </ul>
+        </li>
+        <li>
+            <a class="nav-link" href="">
+                <i class="fas fa-credit-card"></i>
+                <span>Payments</span>
+            </a>
+        </li>
+        <li class="menu-header">Pengaturan</li>
+        <li>
+            <a class="nav-link" href="">
+                <i class="fas fa-cog"></i>
+                <span>Setting</span>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link text-danger" href="{{ base_url('auth/logout') }}">
+                <i class="fas fa-sign-out-alt"></i>
+                <span>Logout</span>
+            </a>
+        </li>
+    </ul>
+</aside>
